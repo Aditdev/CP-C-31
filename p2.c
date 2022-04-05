0e282bdb50858cd8870c0ba827e13be523dd07cb
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-04-05 12:59:00 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,24 @@
+// Tokens-> Data types
+#include <stdio.h>
+int main()
+{
+    int a = 8;
+    double const b = 4.786;
+    float c=1.8;
+    char d ='Z';
+    
+    // Display
+    
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %lf \n", b);
+    printf("The value of c is %0.2f \n", c);
+    printf("The value of d is %c\n", d);
+    return 0;
+} 
+
+// Output
+// The value of a is 8
+// The value of b is 4.786000
+// The value of c is 1.80
+// The value of d is Z
+
