1001fb45dac80330e4415e86cb2429703923a8b1
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-04-12 13:06:02 +0530

    Add files via upload

---
+++ C-31-P6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int a,b,n;
+int main(){
+    printf("Enter two Numbers: ");
+    scanf("%d%d",&a,&b);
+    n=a+b;
+    printf("Sum of %d and %d is %d\n",a,b,n);
+    if (n>20){
+        printf("Sum is greater than 20");
+    }
+    else if (n>10 && n<20) {
+        printf("Sum is between 10 and 20");
+    }
+    else if (n<10 && n>0){
+        printf("Sum is less than 10");
+    }
+    return 0;
+}
+
+//Output:
+//Enter two Numbers : 67 78
+//Sum of 67 and 78 is 145
+//Sum is greatr than 20
