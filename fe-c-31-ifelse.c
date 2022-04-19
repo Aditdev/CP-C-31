78362c8fe0b6e678fc3fc7f839cb85ccca91a68d
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-04-19 11:48:55 +0530

    Add files via upload

---
+++ fe-c-31-ifelse.c
@@ -0,0 +1,20 @@
+// A program to know the use of if-else ladder
+// A program to know the range in which a number lies
+#include <stdio.h>
+
+int main(){
+    int ch, x;
+    printf("Enter a number in the range 0 to 39:\n");
+    scanf("%d", &x);
+    ch = x/10;
+    if (ch == 0)
+            printf("%d is in the range of 0 to 9", x);
+    else if (ch == 1)
+            printf("%d is in the range of 10 to 19", x);
+    else if (ch == 2)
+            printf("%d is in the range of 20 to 29", x);
+    else if (ch == 3)
+            printf("%d is in the range of 30 to 39", x);
+    else
+            printf("%d is out of range", x);
+}
---
+++ fe-c-31-switch.c
@@ -0,0 +1,28 @@
+// A program to know the use of switch statement
+// A program to know the range in which a number lies
+#include <stdio.h>
+
+int main(){
+    int ch, x ;
+    printf("Enter a number in the range 0 to 39:\n");
+    scanf("%d", &x);
+    ch = x/10;
+    switch(ch){
+        case 0:
+            printf("%d is in the range of 0 to 9",x);
+            break;
+        case 1:
+            printf("%d is in the range of 10 to 19",x);
+            break;
+        case 2:
+            printf("%d is in the range of 20 to 29",x);
+            break;
+        case 3:
+            printf("%d is in the range of 30 to 39",x);
+            break;
+        default:
+            printf("%d is out of range",x);
+            break;
+        
+    }
+}
