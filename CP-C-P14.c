4c326f28878e6888f7eb6b7d1ef825b7e56459c6
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-04-26 12:35:40 +0530

    Add files via upload

---
+++ CP-C-P14.c
@@ -0,0 +1,45 @@
+// 2D Array with function;
+
+
+
+#include <stdio.h>
+
+// Fumction to display 
+int display (int arr[3][3])
+{
+  int i, j;
+  for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+    	printf ("%d\t", arr[i][j]);
+    printf("\n\n");
+    }
+    return 0;
+
+}
+
+// Main Function
+
+int main ()
+{
+  int arr1[3][3], arr2[3][3];
+  int i = 0, j = 0;
+  printf("Enter the values of 1st 2d 3x3 Array\n");
+  for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+        scanf ("%d", &arr1[i][j]);
+    printf ("\n");
+    }
+  printf("Enter the values of 2nd 2d 3x3 Array\n");
+    for (i = 0; i < 3; i++)
+    {
+      for (j = 0; j < 3; j++)
+        scanf ("%d", &arr2[i][j]);
+    printf ("\n");
+    }
+    display(arr1);
+    printf("\n");
+    display(arr2);
+  return 0;
+}
