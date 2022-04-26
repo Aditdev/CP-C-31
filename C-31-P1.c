b65a321a24480381051905adb834ac16cbeb9a4d
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-04-26 12:58:44 +0530

    Update and rename p1.c to C-31-P1.c

---
+++ C-31-P1.c
@@ -0,0 +1,21 @@
+//My first C Program
+
+#include <stdio.h> // Header file 
+
+int main() //Main function return type in integer
+
+{
+    
+	printf("Talk to a teacher\n");
+    
+	printf("Welcome");
+    
+	return 0; // function return type is int so it returns 0
+
+}
+
+
+//Output 
+//Talk to a teacher
+//Welcome
+
