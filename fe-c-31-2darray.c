22fb6bd077213632d94e4a9594ff9bbc08869953
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-05-24 11:46:29 +0530

    Update fe-c-31-2darray.c

--- fe-c-31-2darray.c
+++ fe-c-31-2darray.c
@@ -8,14 +8,16 @@ int main(){
         for (j=0;j<3;j++)
         scanf("%d",&arr[i][j]);
     }
+    printf("\nEnter another Number array");
     for(i=0;i<3;i++){
         for (j=0;j<3;j++)
         scanf("%d",&arr2[i][j]);
     }
+    printf("The Sum of both the Matries is");
     for(i=0;i<3;i++){
-        printf("\n")
-        ;for (j=0; j<3 ; j++ ) 
-        printf("%3d " , arr[i][j] + arr2[i][j] );
+        printf("\n");
+        for (j=0; j<3 ; j++ ) 
+        printf("%3d" , arr[i][j] + arr2[i][j] );
     }
     return 0;
     
